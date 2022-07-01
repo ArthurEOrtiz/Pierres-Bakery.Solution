13cc643e3906db89b6d565cc5f6cc3d09a694331
ArthurEOrtiz <ortiz.arthur.e@gmail.com>
2022-07-01 15:23:40 -0600

    Initial Fail Test
    correctly compile for unit testing

---
+++ PierresBakery.Tests/ModelTests/MenuItems.Tests.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PierresBakery;
+
+namespace PoerresBakery.Tests
+{
+  [TestClass]
+  public class MenuItemsTests
+  {
+    [TestMethod]
+    public void BreadList_ReturnListOfBread_BreadListItems()
+    {
+    //Tests will go in here
+    }
+  }
+}
---
+++ PierresBakery/Models/MenuItems.cs
@@ -0,0 +1,9 @@
+using System.Collections.Generic;
+
+namespace PierresBakery // logic goes here
+{
+  public class MenuItems // is the class 
+  {
+    // in here  goes the method. 
+  }
+}
