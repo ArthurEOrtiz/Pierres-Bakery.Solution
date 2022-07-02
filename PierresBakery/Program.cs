4d08a4f29e14c154f50d87f3c81d3810bcca9f5f
ArthurEOrtiz <ortiz.arthur.e@gmail.com>
2022-07-01 18:35:05 -0600

    Add program file
    Added Program file to visualize testing.

---
+++ PierresBakery/Program.cs
@@ -0,0 +1,13 @@
+using System;
+using PierresBakery;
+
+namespace PierresBakery
+{
+  public class Program
+  {
+    public static void Main()
+    {
+      Console.WriteLine("Program is running...");
+    }
+  }
+}
