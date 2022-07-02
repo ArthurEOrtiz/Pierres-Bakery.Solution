c1e987c776a0dc94887f9ff3a3566344f89e2c69
ArthurEOrtiz <ortiz.arthur.e@gmail.com>
2022-07-01 19:23:05 -0600

    Console Write
    add console write lines to visualizes getters and setters

--- PierresBakery/Models/MenuItems.cs
+++ PierresBakery/Models/MenuItems.cs
@@ -4,10 +4,24 @@ namespace PierresBakery // logic goes here
 {
   public class Bread// is the class 
   {
+    private int _quantity;
+    private int _price;
     // in here  goes the method.
     public Bread(int quantity, int price)
     {
+      _quantity = quantity;
+      _price = price;
 
+    }
+
+    public int GetQuantity()
+    {
+      return _quantity;
+    }
+
+    public int GetPrice()
+    {
+      return _price;
     }
   
   }
--- PierresBakery/Program.cs
+++ PierresBakery/Program.cs
@@ -8,6 +8,10 @@ namespace PierresBakery
     public static void Main()
     {
       Console.WriteLine("Program is running...");
+      Bread newBread = new Bread(1, 1);
+      Console.WriteLine(newBread); // this outputs PierresBakkery.Bread
+      Console.WriteLine(newBread.GetPrice()); // this returns 1
+      Console.WriteLine(newBread.GetQuantity()); // this returns 1
     }
   }
 }
