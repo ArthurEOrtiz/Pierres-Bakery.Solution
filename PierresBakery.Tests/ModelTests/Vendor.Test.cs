b46c21fc66a908c20bd4e350763a8aeb4e512e15
ArthurEOrtiz <ortiz.arthur.e@gmail.com>
2022-07-15 17:16:55 -0600

    Created Vendor
    Created and Tested an instance of vendor.

---
+++ PierresBakery.Tests/ModelTests/Vendor.Test.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PierresBakery;
+
+namespace PierresBakery.Tests 
+{
+  [TestClass]
+  public class VendorTests
+  {
+    [TestMethod]
+    public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
+    {
+      Vendor newVendor = new Vendor();
+      Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    }
+  }
+}
---
+++ PierresBakery/Models/Vendor.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace PierresBakery
+{
+  public class Vendor
+  {
+    // public string Name { get; set; }
+    // public string Description { get; set; }
+    // public int Id {get; }
+    // private static List<Vendor> Vendors = new List<Vendor> {};
+
+
+  }
+}
