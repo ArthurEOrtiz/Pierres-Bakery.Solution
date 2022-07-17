151b1712b31e920d3a71cd990b7a8c8f8a7f636e
ArthurEOrtiz <ortiz.arthur.e@gmail.com>
2022-07-16 20:20:00 -0600

    Able to add vendors to list

--- PierresBakery/Controllers/VendorsController.cs
+++ PierresBakery/Controllers/VendorsController.cs
@@ -8,11 +8,24 @@ namespace PierresBakery.Controllers
   public class VendorsController : Controller
   {
 
-    [HttpGet("/vendor")]
+    [HttpGet("/vendors")]
     public ActionResult Index()
     {
       List<Vendor> allVendors = Vendor.GetAll();
       return View(allVendors);
+    }
+
+    [HttpGet("/vendors/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+
+    [HttpPost("/vendors")]
+    public ActionResult Create(string name, string description)
+    {
+      Vendor myVendor = new Vendor(name, description);
+      return RedirectToAction("Index", myVendor);
     }
 
   }
